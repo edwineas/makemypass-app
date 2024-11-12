5b6afcbe29099c47b03632e0653927297fc9aeed
M Viraj Prabhu <virajprabhup@gmail.com>
2024-11-12 15:00:00 +0530

    fix(auth): added-profile-pic-url-to-reset-password

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -171,6 +171,7 @@ export const loginUsingGoogle = async (
     localStorage.setItem('accessToken', response.data.response.access_token);
     localStorage.setItem('refreshToken', response.data.response.refresh_token);
     localStorage.setItem('userEmail', response.data.response.email);
+    localStorage.setItem('userImage', response.data.response.profile_pic_url);
     setIsAuthenticated(true);
     onboardUser();
 
