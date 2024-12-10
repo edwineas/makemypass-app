2b60879396a70b98250c96807d4f55cfc2f3d276
M Viraj Prabhu <virajprabhup@gmail.com>
2024-12-10 14:21:05 +0530

    fix(Events): added-participated-events

---
+++ src/pages/app/UserEventInfo/UserEventInfo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Theme from '../../../components/Theme/Theme';
+
+type Props = {};
+
+const UserEventInfo = (props: Props) => {
+  return <Theme>UserEventInfo</Theme>;
+};
+
+export default UserEventInfo;
